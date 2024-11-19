b846b6df3193a5a6d7e5ecff3bdd932249544fad
Henry <henry.eguia@kodakollectiv.com>
2024-11-19 21:54:59 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+void main() {
+  runApp(const DrawingApp());
+}
+
+class DrawingApp extends StatelessWidget {
+  const DrawingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final GoRouter router = GoRouter(
+      initialLocation: '/',
+      routes: <RouteBase>[
+        GoRoute(
+          path: '/',
+          builder: (BuildContext context, GoRouterState state) {
+            return Container();
+          },
+        ),
+      ],
+    );
+
+    return MaterialApp.router(
+      routerConfig: router,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+      ],
+    );
+  }
+}
