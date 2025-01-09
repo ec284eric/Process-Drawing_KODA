de2469b00dc26dfd09789773a3aecff6823cdd3d
henryEguia <henry.eguia@kodakollectiv.com>
2025-01-09 17:11:33 +0800

    fix: change picture orientation

--- lib/draw/widgets/overlay_picker_bottom_sheet.dart
+++ lib/draw/widgets/overlay_picker_bottom_sheet.dart
@@ -42,13 +42,13 @@ class OverlayPickerBottomSheet extends StatelessWidget {
                 ),
                 InkWell(
                   onTap: () => onImageSelected?.call(
-                      'https://static.vecteezy.com/system/resources/thumbnails/008/951/892/small_2x/cute-puppy-pomeranian-mixed-breed-pekingese-dog-run-on-the-grass-with-happiness-photo.jpg'),
+                      'https://t4.ftcdn.net/jpg/02/66/72/41/360_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg'),
                   child: Transform(
                     alignment: Alignment.center,
                     transform: Matrix4.identity()
                       ..scale(-1.0, 1.0), // Flip horizontally
                     child: Image.network(
-                      'https://static.vecteezy.com/system/resources/thumbnails/008/951/892/small_2x/cute-puppy-pomeranian-mixed-breed-pekingese-dog-run-on-the-grass-with-happiness-photo.jpg',
+                      'https://t4.ftcdn.net/jpg/02/66/72/41/360_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg',
                       fit: BoxFit.cover,
                       width: MediaQuery.of(context).size.width,
                     ),
