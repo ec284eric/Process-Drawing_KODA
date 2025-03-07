a7bc63d79d106099ba27fda24c0de34ac228966c
JasperKODA <jasper.besinga@kodakollectiv.com>
2025-03-07 17:49:50 +0800

    refactor: comment unused import

--- lib/draw/view/color_picker.dart
+++ lib/draw/view/color_picker.dart
@@ -1,6 +1,6 @@
 import 'package:drawing_app/draw/bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flex_color_picker/flex_color_picker.dart' as flex_color_picker;
+// import 'package:flex_color_picker/flex_color_picker.dart' as flex_color_picker;
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ColorPicker extends StatelessWidget {
