8b08858d7916d0d9778e73ce2b1b4e89ebf47ffd
JasperKODA <jasper.besinga@kodakollectiv.com>
2025-03-07 18:18:47 +0800

    refactor: remove reflected drawing scaling; remove reflected image sizing; duplicated selected image for flipping

--- lib/draw/bloc/draw_bloc.dart
+++ lib/draw/bloc/draw_bloc.dart
@@ -64,7 +64,25 @@ class DrawBloc extends Bloc<DrawEvent, DrawState> {
 
   void _imageFlippedIconPressed(
       ImageFlippedIconPressed event, Emitter<DrawState> emit) {
-    emit(state.copyWith(imageFlipped: !state.imageFlipped));
+    final modifiableImages =
+        List<ModifiableImage?>.from(state.modifiableImages);
+
+    while (modifiableImages.length < 2) {
+      modifiableImages.add(null);
+    }
+
+    if (modifiableImages[1] == null && modifiableImages[0] != null) {
+      modifiableImages[1] = modifiableImages[0]!.copyWith(
+        offset: modifiableImages[0]!.offset,
+        scale: modifiableImages[0]!.scale,
+        rotation: modifiableImages[0]!.rotation,
+      );
+    }
+
+    emit(state.copyWith(
+      modifiableImages: modifiableImages,
+      imageFlipped: !state.imageFlipped,
+    ));
   }
 
   void _drawingIconPresed(DrawingIconPresed event, Emitter<DrawState> emit) {
@@ -111,7 +129,7 @@ class DrawBloc extends Bloc<DrawEvent, DrawState> {
         rotation: event.details.rotation,
       );
     }
-    if (event.details.scale != 1) {
+    if (event.details.scale != 1 && event.index == 0) {
       modifiableImages[event.index] = modifiableImages[event.index]?.copyWith(
         scale: event.details.scale,
       );
@@ -249,21 +267,23 @@ class DrawBloc extends Bloc<DrawEvent, DrawState> {
       DrawReflectedImageScaleUpdated event, Emitter<DrawState> emit) {
     var modifiableImage = state.reflectedImage;
     var rotation = state.rotation + event.details.rotation;
+
     if ((rotation - state.rotation).abs() > 0) {
       modifiableImage = modifiableImage.copyWith(
         rotation: event.details.rotation,
       );
     }
-    if (event.details.scale != 1) {
-      modifiableImage = modifiableImage.copyWith(
-        scale: event.details.scale,
-      );
-      emit(
-        state.copyWith(
-          reflectedImage: modifiableImage,
-        ),
-      );
-    }
+    // Remove reflected drawing scaled
+    // if (event.details.scale != 1) {
+    //   modifiableImage = modifiableImage.copyWith(
+    //     scale: event.details.scale,
+    //   );
+    //   emit(
+    //     state.copyWith(
+    //       reflectedImage: modifiableImage,
+    //     ),
+    //   );
+    // }
     modifiableImage = modifiableImage.copyWith(
       offset: (state.reflectedImage.offset) + event.details.focalPointDelta,
     );
