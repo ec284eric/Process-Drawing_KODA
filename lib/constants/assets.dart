ddbb7ca7d59615f81922c722006b8ad67198dfa8
JasperKODA <jasper.besinga@kodakollectiv.com>
2025-03-17 18:11:53 +0800

    feat: create assets

---
+++ lib/constants/assets.dart
@@ -0,0 +1,8 @@
+class Assets {
+  static const acetateMontageImg1 = 'assets/images/image_01.jpg';
+  static const acetateMontageImg2 = 'assets/images/image_02.jpg';
+  static const acetateMontageImg3 = 'assets/images/image_03.jpg';
+  static const acetateMontageImg4 = 'assets/images/image_04.jpg';
+  static const acetateMontageImg5 = 'assets/images/image_05.jpg';
+  static const acetateMontageImg6 = 'assets/images/image_06.jpg';
+}
