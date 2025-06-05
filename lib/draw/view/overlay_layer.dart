5d17ca81d2d3610eee7311cf586b61cab7292fa9
MAEMABILOG <mae.mabilog@kodakollectiv.com>
2025-06-05 17:32:04 +0800

    feat: add InteractiveViewer with transformationController

--- lib/draw/view/overlay_layer.dart
+++ lib/draw/view/overlay_layer.dart
@@ -23,31 +23,34 @@ class OverlayLayer extends StatelessWidget {
               return SizedBox(
                 height: constraint.maxHeight,
                 width: constraint.maxWidth,
-                child: Stack(
-                  children: state.modifiableImages
-                      .mapIndexed((index, modifiableImage) {
-                    if (index == 0 && modifiableImage != null) {
-                      return ModifiableImageItem(
-                        modifiableImage: modifiableImage,
-                        opacity: 1,
-                        onScaleUpdate: (details) => bloc.add(
-                          DrawImageScaleUpdated(index, details),
-                        ),
-                        secondImage: false,
-                      );
-                    } else if (index == 1 && modifiableImage != null) {
-                      return ModifiableImageItem(
-                        modifiableImage: modifiableImage,
-                        opacity: 0.8,
-                        onScaleUpdate: (details) => bloc.add(
-                          DrawImageScaleUpdated(index, details),
-                        ),
-                        secondImage: state.imageFlipped ? true : false,
-                      );
-                    } else {
-                      return Container();
-                    }
-                  }).toList(),
+                child: InteractiveViewer(
+                  transformationController: transformationController,
+                  child: Stack(
+                    children: state.modifiableImages
+                        .mapIndexed((index, modifiableImage) {
+                      if (index == 0 && modifiableImage != null) {
+                        return ModifiableImageItem(
+                          modifiableImage: modifiableImage,
+                          opacity: 1,
+                          onScaleUpdate: (details) => bloc.add(
+                            DrawImageScaleUpdated(index, details),
+                          ),
+                          secondImage: false,
+                        );
+                      } else if (index == 1 && modifiableImage != null) {
+                        return ModifiableImageItem(
+                          modifiableImage: modifiableImage,
+                          opacity: 0.8,
+                          onScaleUpdate: (details) => bloc.add(
+                            DrawImageScaleUpdated(index, details),
+                          ),
+                          secondImage: state.imageFlipped ? true : false,
+                        );
+                      } else {
+                        return Container();
+                      }
+                    }).toList(),
+                  ),
                 ),
               );
             },
