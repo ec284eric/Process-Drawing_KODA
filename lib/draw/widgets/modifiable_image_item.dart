c28447e0ae2b92bdcf5bd3d75eab1defb4ffec07
MAEMABILOG <mae.mabilog@kodakollectiv.com>
2025-06-17 16:31:38 +0800

    refactor: update widget

--- lib/draw/widgets/modifiable_image_item.dart
+++ lib/draw/widgets/modifiable_image_item.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:drawing_app/draw/models/models.dart';
 import 'package:flutter/material.dart';
 
@@ -29,25 +31,30 @@ class ModifiableImageItem extends StatelessWidget {
           child: Transform.rotate(
             angle: modifiableImage.rotation,
             child: Opacity(
-              opacity: opacity,
-              child: secondImage == true
-                  ? Transform(
-                      alignment: Alignment.center,
-                      transform: Matrix4.identity()..scale(-1.0, 1.0),
-                      child: Opacity(
-                        opacity: opacity,
-                        child: Image.asset(
-                          modifiableImage.src,
-                          fit: BoxFit.contain,
-                          width: MediaQuery.of(context).size.width,
-                        ),
-                      ))
-                  : Image.asset(
-                      modifiableImage.src,
-                      fit: BoxFit.contain,
-                      width: MediaQuery.of(context).size.width,
-                    ),
-            ),
+                opacity: opacity,
+                child: secondImage == true
+                    ? Transform(
+                        alignment: Alignment.center,
+                        transform: Matrix4.identity()..scale(-1.0, 1.0),
+                        child: Opacity(
+                          opacity: opacity,
+                          child: modifiableImage.src != null
+                              ? Image.asset(
+                                  modifiableImage.src ?? '',
+                                  fit: BoxFit.contain,
+                                  width: MediaQuery.of(context).size.width,
+                                )
+                              : Image.memory(
+                                  modifiableImage.imageBytes ?? Uint8List(0),
+                                  fit: BoxFit.contain,
+                                  width: MediaQuery.of(context).size.width,
+                                ),
+                        ))
+                    : Image.asset(
+                        modifiableImage.src ?? '',
+                        fit: BoxFit.contain,
+                        width: MediaQuery.of(context).size.width,
+                      )),
           ),
         ),
       ),
