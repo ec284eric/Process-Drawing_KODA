7fa1dc95c766125c37031ce2d28dccfed87789de
MAEMABILOG <mae.mabilog@kodakollectiv.com>
2025-06-17 16:30:36 +0800

    refactor: update model

--- lib/draw/models/modifiable_image.dart
+++ lib/draw/models/modifiable_image.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -6,7 +8,8 @@ part 'modifiable_image.freezed.dart';
 @freezed
 class ModifiableImage with _$ModifiableImage {
   const factory ModifiableImage({
-    required String src,
+    String? src,
+    Uint8List? imageBytes,
     @Default(0) double rotation,
     @Default(1) double scale,
     @Default(Offset(0, 0)) Offset offset,
