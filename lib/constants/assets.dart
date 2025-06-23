f583238a2a6dff681448ef01e0a9271009cd74f6
MAEMABILOG <mae.mabilog@kodakollectiv.com>
2025-06-23 10:39:31 +0800

    refactor: update app icon

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -6,7 +6,7 @@ class Assets {
   static const acetateMontageImg5 = 'assets/images/image_05.jpg';
   static const acetateMontageImg6 = 'assets/images/image_06.jpg';
   static const appIcon = 'assets/icons/appicon_process.png';
-  static const appIconWellness = 'assets/icons/appicon_wellness.png';
+  static const appIconWellness = 'assets/icons/wellness.png';
   static const colorIcon = 'assets/icons/color-01.png';
   static const copyDrawingIcon = 'assets/icons/copy-drawing-01.png';
   static const downloadIcon = 'assets/icons/download-01.png';
