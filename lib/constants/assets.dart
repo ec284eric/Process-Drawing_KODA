ae06a664a1e3bdc2ee725c52b461e8c4905a78db
MAEMABILOG <mae.mabilog@kodakollectiv.com>
2025-07-24 14:37:04 +0800

    refactor: change to png

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -1,10 +1,10 @@
 class Assets {
-  static const acetateMontageImg1 = 'assets/images/image_01.jpg';
-  static const acetateMontageImg2 = 'assets/images/image_02.jpg';
-  static const acetateMontageImg3 = 'assets/images/image_03.jpg';
-  static const acetateMontageImg4 = 'assets/images/image_04.jpg';
-  static const acetateMontageImg5 = 'assets/images/image_05.jpg';
-  static const acetateMontageImg6 = 'assets/images/image_06.jpg';
+  static const acetateMontageImg1 = 'assets/images/image_01.png';
+  static const acetateMontageImg2 = 'assets/images/image_02.png';
+  static const acetateMontageImg3 = 'assets/images/image_03.png';
+  static const acetateMontageImg4 = 'assets/images/image_04.png';
+  static const acetateMontageImg5 = 'assets/images/image_05.png';
+  static const acetateMontageImg6 = 'assets/images/image_06.png';
   static const appIcon = 'assets/icons/appstore.png';
   static const appIconWellness = 'assets/icons/appicon_wellness.png';
   static const colorIcon = 'assets/icons/color-01.png';
