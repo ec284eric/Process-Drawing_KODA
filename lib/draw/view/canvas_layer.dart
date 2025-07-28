3bd9522e199397f06bb3ecbb313ee4c8407ffca8
MAEMABILOG <mae.mabilog@kodakollectiv.com>
2025-07-28 18:09:09 +0800

    refactor: change to if else condition

--- lib/draw/view/canvas_layer.dart
+++ lib/draw/view/canvas_layer.dart
@@ -72,130 +72,134 @@ class _CanvasLayerState extends State<CanvasLayer> {
                 },
                 onPointerUp: (pue) {},
                 background: SizedBox(
-                  child: SizedBox(
-                    width: constraints.maxWidth,
-                    height: constraints.maxHeight,
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Visibility(
-                          visible: state.locked &&
-                              state.imageCollectRequestStatus !=
-                                  RequestStatus.inProgress &&
-                              (state.showBackground || !state.isToggled),
-                          maintainState: true,
-                          maintainAnimation: true,
-                          maintainSize: true,
-                          child: Stack(
-                            children: [
-                              ...state.modifiableImages.mapIndexed(
-                                (index, modifiableImage) {
-                                  if (modifiableImage != null) {
-                                    return IgnorePointer(
-                                      ignoring: state.canDraw,
-                                      child: ModifiableImageItem(
-                                        modifiableImage: modifiableImage,
-                                        opacity: state.locked ? 0.5 : 0.9,
-                                        onScaleUpdate: (details) => bloc.add(
-                                          DrawImageScaleUpdated(
-                                            index: index,
-                                            details: details,
-                                          ),
+                  width: constraints.maxWidth,
+                  height: constraints.maxHeight,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Visibility(
+                        visible: state.locked &&
+                            state.imageCollectRequestStatus !=
+                                RequestStatus.inProgress &&
+                            (state.showBackground || !state.isToggled),
+                        maintainState: true,
+                        maintainAnimation: true,
+                        maintainSize: true,
+                        child: Stack(
+                          children: [
+                            ...state.modifiableImages.mapIndexed(
+                              (index, modifiableImage) {
+                                if (modifiableImage != null) {
+                                  return IgnorePointer(
+                                    ignoring: state.canDraw,
+                                    child: ModifiableImageItem(
+                                      modifiableImage: modifiableImage,
+                                      opacity: state.locked ? 0.5 : 0.9,
+                                      onScaleUpdate: (details) => bloc.add(
+                                        DrawImageScaleUpdated(
+                                          index: index,
+                                          details: details,
                                         ),
-                                        onScaleEnd: () => bloc.add(
-                                          DrawGestureEnded(
-                                            index: index,
-                                          ),
-                                        ),
-                                        secondImage:
-                                            index == 1 && state.imageFlipped,
                                       ),
-                                    );
-                                  } else {
-                                    return Container();
-                                  }
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-                        Visibility(
-                          visible: state.drawingFlipped,
-                          child: SizedBox(
-                            width: constraints.maxWidth,
-                            height: constraints.maxHeight,
-                            child: ModifiableImageItem(
-                              modifiableImage: state.reflectedImage,
-                              onScaleUpdate: state.locked
-                                  ? (value) => bloc.add(
-                                        DrawReflectedImageScaleUpdated(
-                                          details: value,
+                                      onScaleEnd: () => bloc.add(
+                                        DrawGestureEnded(
+                                          index: index,
                                         ),
-                                      )
-                                  : null,
-                              secondImage: true,
+                                      ),
+                                      secondImage:
+                                          index == 1 && state.imageFlipped,
+                                    ),
+                                  );
+                                } else {
+                                  return Container();
+                                }
+                              },
                             ),
+                          ],
+                        ),
+                      ),
+                      Visibility(
+                        visible: state.drawingFlipped,
+                        child: SizedBox(
+                          width: constraints.maxWidth,
+                          height: constraints.maxHeight,
+                          child: ModifiableImageItem(
+                            modifiableImage: state.reflectedImage,
+                            onScaleUpdate: state.locked
+                                ? (value) => bloc.add(
+                                      DrawReflectedImageScaleUpdated(
+                                        details: value,
+                                      ),
+                                    )
+                                : null,
+                            secondImage: true,
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
             );
 
-            return Stack(
-              children: [
-                state.locked
-                    ? GestureDetector(
-                        onScaleStart: (details) {
-                          _initialFocalPoint = details.focalPoint;
-                          _initialMatrix =
-                              widget.transformationController.value.clone();
-                        },
-                        onScaleUpdate: (details) {
-                          const double rotationSensitivity = 0.3;
-                          const double scaleSensitivity = 0.3;
+            if (state.locked) {
+              return Stack(
+                children: [
+                  GestureDetector(
+                    onScaleStart: (details) {
+                      _initialFocalPoint = details.focalPoint;
+                      _initialMatrix =
+                          widget.transformationController.value.clone();
+                    },
+                    onScaleUpdate: (details) {
+                      const double rotationSensitivity = 0.3;
+                      const double scaleSensitivity = 0.3;
 
-                          final Offset focalPointDelta =
-                              details.focalPoint - _initialFocalPoint;
+                      final Offset focalPointDelta =
+                          details.focalPoint - _initialFocalPoint;
 
-                          final double adjustedScale =
-                              1.0 + ((details.scale - 1.0) * scaleSensitivity);
+                      final double adjustedScale =
+                          1.0 + ((details.scale - 1.0) * scaleSensitivity);
 
-                          Matrix4 newMatrix = _initialMatrix.clone();
+                      Matrix4 newMatrix = _initialMatrix.clone();
 
-                          newMatrix = Matrix4.identity()
-                            ..translate(focalPointDelta.dx, focalPointDelta.dy)
-                            ..multiply(newMatrix);
+                      newMatrix = Matrix4.identity()
+                        ..translate(focalPointDelta.dx, focalPointDelta.dy)
+                        ..multiply(newMatrix);
 
-                          if (details.scale != 1.0 || details.rotation != 0.0) {
-                            final Matrix4 transform = Matrix4.identity()
-                              ..translate(
-                                  details.focalPoint.dx, details.focalPoint.dy)
-                              ..rotateZ(details.rotation * rotationSensitivity)
-                              ..scale(adjustedScale)
-                              ..translate(-details.focalPoint.dx,
-                                  -details.focalPoint.dy);
+                      if (details.scale != 1.0 || details.rotation != 0.0) {
+                        final Matrix4 transform = Matrix4.identity()
+                          ..translate(
+                              details.focalPoint.dx, details.focalPoint.dy)
+                          ..rotateZ(details.rotation * rotationSensitivity)
+                          ..scale(adjustedScale)
+                          ..translate(
+                              -details.focalPoint.dx, -details.focalPoint.dy);
 
-                            newMatrix = transform..multiply(newMatrix);
-                          }
+                        newMatrix = transform..multiply(newMatrix);
+                      }
 
-                          widget.transformationController.value = newMatrix;
-                        },
-                        child: Transform(
-                          transform: widget.transformationController.value,
-                          child: Container(
-                            color: Colors.transparent,
-                            width: double.infinity,
-                            height: double.infinity,
-                            child: drawingBoard,
-                          ),
-                        ),
-                      )
-                    : drawingBoard,
-              ],
-            );
+                      widget.transformationController.value = newMatrix;
+                    },
+                    child: Transform(
+                      transform: widget.transformationController.value,
+                      child: Container(
+                        color: Colors.transparent,
+                        width: double.infinity,
+                        height: double.infinity,
+                        child: drawingBoard,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return Stack(
+                children: [
+                  drawingBoard,
+                ],
+              );
+            }
           },
         );
       },
