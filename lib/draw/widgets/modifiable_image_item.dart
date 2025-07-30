97ad2112154dad3fb94ac6af40e266772c8b9e17
MAEMABILOG <mae.mabilog@kodakollectiv.com>
2025-07-30 17:11:02 +0800

    refactor: adjust montage image size to prevent clipping when rotated

--- lib/draw/widgets/modifiable_image_item.dart
+++ lib/draw/widgets/modifiable_image_item.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 class ModifiableImageItem extends StatelessWidget {
   final ModifiableImage modifiableImage;
   final ValueChanged<ScaleUpdateDetails>? onScaleUpdate;
-  final VoidCallback? onScaleEnd; // <-- Add this line
+  final VoidCallback? onScaleEnd;
 
   final double opacity;
   final bool secondImage;
@@ -15,42 +15,55 @@ class ModifiableImageItem extends StatelessWidget {
     super.key,
     required this.modifiableImage,
     this.onScaleUpdate,
-    this.onScaleEnd, // <-- Add this to the constructor
+    this.onScaleEnd,
     this.opacity = 1,
     required this.secondImage,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onScaleUpdate: onScaleUpdate,
-      child: Transform.rotate(
-        angle: modifiableImage.rotation,
-        child: Opacity(
-            opacity: opacity,
-            child: secondImage == true
-                ? Transform(
-                    alignment: Alignment.center,
-                    transform: Matrix4.identity()..scale(-1.0, 1.0),
-                    child: Opacity(
-                      opacity: opacity,
-                      child: modifiableImage.src != null
-                          ? Image.asset(
-                              modifiableImage.src ?? '',
-                              fit: BoxFit.contain,
-                              width: MediaQuery.of(context).size.width,
-                            )
-                          : Image.memory(
-                              modifiableImage.imageBytes ?? Uint8List(0),
-                              fit: BoxFit.contain,
-                              width: MediaQuery.of(context).size.width,
-                            ),
-                    ))
-                : Image.asset(
-                    modifiableImage.src ?? '',
-                    fit: BoxFit.contain,
-                    width: MediaQuery.of(context).size.width,
-                  )),
+    final imageWidth = modifiableImage.originalSize?.width;
+    final imageHeight = modifiableImage.originalSize?.height ?? 550;
+
+    final imageWidget = modifiableImage.src != null
+        ? Image.asset(
+            modifiableImage.src!,
+            fit: BoxFit.contain,
+          )
+        : Image.memory(
+            modifiableImage.imageBytes ?? Uint8List(0),
+            fit: BoxFit.contain,
+          );
+
+    return Transform.translate(
+      offset: modifiableImage.offset,
+      child: Transform.scale(
+        scale: modifiableImage.scale,
+        child: GestureDetector(
+          onScaleUpdate: onScaleUpdate,
+          child: Transform.rotate(
+            angle: modifiableImage.rotation,
+            child: Opacity(
+              opacity: opacity,
+              child: Center(
+                child: SizedBox(
+                  width: imageWidth,
+                  height: imageHeight,
+                  child: secondImage
+                      ? Transform(
+                          alignment: Alignment.center,
+                          transform: Matrix4.identity()..scale(-1.0, 1.0),
+                          child: Opacity(
+                            opacity: opacity,
+                            child: imageWidget,
+                          ),
+                        )
+                      : imageWidget,
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
