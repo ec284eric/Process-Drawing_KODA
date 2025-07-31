77ffc89ef05f50badf27c6269eeb4a5e44f2f520
jaokoda <japherson.maloloy-on@kodakollectiv.com>
2025-07-31 16:54:12 +0800

    refactor: adjust basestroke

--- lib/draw/bloc/draw_bloc.dart
+++ lib/draw/bloc/draw_bloc.dart
@@ -78,7 +78,7 @@ class DrawBloc extends Bloc<DrawEvent, DrawState> {
       penSelector: false,
       canDraw: true,
       strokeWidth: base,
-      baseStrokeWidth: base / (zoom * 0.9),
+      baseStrokeWidth: base / (zoom * 1.1),
     ));
   }
 
